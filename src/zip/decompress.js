182e7b2b6be08763970aa478eac2e74c9fc2423d
Dima <dikashko@gmail.com>
2022-06-05 12:41:21 +0300

    feat: zip complete

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,17 @@
+import { getAbsolutePath } from "../utils/path.js";
+import * as fs from "fs";
+import { fileURLToPath } from "url";
+import * as zlib from "zlib";
+
+const __filename = fileURLToPath(import.meta.url);
+
 export const decompress = async () => {
-    // Write your code here 
+    const filePath = getAbsolutePath(__filename, "fileToCompress.txt");
+    const archivePath = getAbsolutePath(__filename, "archive.gz");
+    const fileStream = fs.createReadStream(archivePath);
+    const archiveStream = fs.createWriteStream(filePath);
+    const gzip = zlib.createGunzip();
+    fileStream.pipe(gzip).pipe(archiveStream);
 };
+
+decompress();
